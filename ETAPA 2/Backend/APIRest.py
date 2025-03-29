5db062aac800a23d5e92d0ccef95eeb3fc555d3d
aarevalom0 <a.arevalom@uniandes.edu.co>
2025-03-29 17:11:24 -0500

    upload first endpoint

--- ETAPA 2/Backend/APIRest.py
+++ ETAPA 2/Backend/APIRest.py
@@ -1,6 +1,9 @@
 from typing import Optional
-
+from joblib import load
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import pandas as pd
+import PredictionModel
 
 app = FastAPI()
 
@@ -15,7 +18,16 @@ def read_item(item_id: int, q: Optional[str] = None):
    return {"item_id": item_id, "q": q}
 
 
-@app.get("/predecir")
-def predecir(documento):
+@app.get("/predecir/{path}")
+def predecir(path:str):
+   path = "../Data/" + path
+   data=pd.read_csv(path, sep=',')
    
-   return 
+   model = PredictionModel.Model()
+   data = model.remove_duplicates(data)
+   data['Titulo'] = model.pipelinePreprocess.transform(data['Titulo'])
+   data['Descripcion'] = model.pipelinePreprocess.transform(data['Descripcion'])
+   X_data=  data['Titulo'].astype(str) + " " + data['Descripcion'].astype(str)
+   vectores = model.Vectorizer(X_data)
+   prediciones=model.make_prediction(vectores)
+   return prediciones
--- ETAPA 2/Backend/PredictionModel.py
+++ ETAPA 2/Backend/PredictionModel.py
@@ -1,12 +1,78 @@
+import unicodedata
+import re
+from num2words import num2words
 from joblib import load
+import pandas as pd
+from sklearn.pipeline import Pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.corpus import stopwords
+
+
+class RemoveNonAscii(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X.apply(lambda text: unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore') if isinstance(text, str) else text)
+
+
+class ToLowercase(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X.str.lower()
+
+
+class RemovePunctuation(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X.apply(lambda text: re.sub(r'[^\w\s]', '', text) if isinstance(text, str) else text)
+
+
+class ReplaceNumbers(BaseEstimator, TransformerMixin):
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X.apply(lambda text: ' '.join([num2words(int(word), lang='es') if word.isdigit() else word for word in text.split()]) if isinstance(text, str) else text)
+
+
+class RemoveStopwords(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        self.stopwords = set(stopwords.words('spanish'))
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X.apply(lambda text: ' '.join([word for word in text.split() if word not in self.stopwords]) if isinstance(text, str) else text)
+
 
 class Model:
+    def __init__(self):
+        self.model = load("../assets/modelo.joblib")
+        
 
-    def __init__(self,columns):
-        self.model = load("assets/modelo.joblib")
-    def preprocess(self, data):
-        return data
-    def make_predictions(self, data):
-        data = self.preprocess(data)
-        result = self.model.predict(data)
-        return result
+        self.pipelinePreprocess = Pipeline([
+            ('remove_non_ascii', RemoveNonAscii()),
+            ('to_lowercase', ToLowercase()),
+            ('remove_punctuation', RemovePunctuation()),
+            ('replace_numbers', ReplaceNumbers()),
+            ('remove_stopwords', RemoveStopwords())
+        ])
+
+    def remove_duplicates(self, df):
+        df.drop_duplicates(subset='Titulo', keep='first', inplace=True)
+        df.drop_duplicates(subset='Descripcion', keep='first', inplace=True)
+        return df
+
+    def Vectorizer(self, data):
+        vectorizer = TfidfVectorizer(max_features=5000, stop_words=stopwords.words('spanish'))
+        return vectorizer.fit_transform(data)
+
+    def make_prediction(self, X_data):
+        return self.model.predict_proba(X_data)
